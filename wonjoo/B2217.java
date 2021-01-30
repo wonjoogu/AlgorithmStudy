26024af344661f2e706d418b094ddfef27c138d4
wonjoo Gu <50662573+wonjoogu@users.noreply.github.com>
2021-01-30 13:42:08 +0900

    210130
    로프

---
+++ wonjoo/B2217.java
@@ -0,0 +1,30 @@
+package wonjoo;
+
+import java.util.Arrays;
+import java.util.Scanner;
+//로프
+public class B2217 {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		int N = sc.nextInt();
+		int[] n = new int[N];
+		
+		
+		for (int i = 0; i < N; i++) {
+			n[i] = sc.nextInt();
+		}
+		
+		Arrays.sort(n);//배열 오름차순 정렬
+		int max = 0;
+
+		for (int i = N - 1; i >= 0; i--) {//배열에서 가장 큰 수부터 꺼냄
+				n[i] *= N-i;// 배열 첫번째 숫자인 10에 2를 곱함
+				max = Math.max(max, n[i]);//0과 20중 최대값 저장
+			}
+		System.out.println(max);//최대값 출력
+	}
+}
+
+
